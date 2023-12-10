b5f6551f4df9f0cab65dca6b96c7807fc8bad390
Marlon Montiel Pérez <mrmarlonmp@gmail.com>
2023-12-09 22:35:33 -0600

    Funcionalidad del Login de la pagina, con sus respectivas verificaciones
    *Faltar asignar a donder redirigir despues del login

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import "./styles/LoginForm.css";
 
-const InputField = ({ label, type, value, onChange }) => (
+const InputField = ({ label, type, value, onChange, isError }) => (
   <Form.Group>
     <Form.Label>{label}</Form.Label>
-    <Form.Control type={type} value={value} onChange={onChange} />
+    <Form.Control 
+      type={type} 
+      value={value} 
+      onChange={onChange} 
+      className={isError ? 'input-error' : ''} 
+    />
   </Form.Group>
 );
 
